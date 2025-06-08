bf9f3f1769f7df4f49b4aa69942d2491986279c1
Joseph Barresi <jbarresi@amazon.com>
2025-06-08 14:48:12 -0400

    Add auth persistence

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const apiKey = process.env.EXPO_PUBLIC_API_KEY;
 
@@ -14,11 +15,13 @@ const firebaseConfig = {
   storageBucket: "recipetok-40c2a.firebasestorage.app",
   messagingSenderId: "280093415880",
   appId: "1:280093415880:web:e5bd0c7e15fa66b50848fd",
-  measurementId: "G-W7K3GRQBZQ"
+  measurementId: "G-W7K3GRQBZQ",
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
 export const FIREBASE_FUNCTIONS = getFunctions(FIREBASE_APP);
